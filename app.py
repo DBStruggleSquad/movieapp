111a64525c03d1a2be75df9997df292fd257f877
Glormar Castro <glorimar.castro@upr.edu>
2015-09-12 14:05:58 -0400

    please

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('helloworld.html')
+    return render_template('try.html')
 
 if __name__ == '__main__':
     app.run()
