5ff86976182782af9111a30e42a7ae07658ac1bc
Glormar Castro <glorimar.castro@upr.edu>
2015-09-12 13:57:41 -0400

    html rendering

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ Created on Sep 12, 2015
 '''
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello"
+    return render_template('helloworld.html')
 
 if __name__ == '__main__':
     app.run()
