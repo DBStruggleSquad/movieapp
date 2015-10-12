59b2e9f8049585533456259b6e53596be9d7e960
kborgos <katya.borgos@upr.edu>
2015-10-11 23:02:01 -0400

    adding feed template

---
+++ static/js/controllers/feedController.js
@@ -0,0 +1,103 @@
+app.controller('feed', ['$scope', '$http', function($scope,$http) {
+  $scope.activities = [{
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }, {
+    publisher: 'katrific',
+    type: 'list',
+    pubdate: new Date('2015', '09', '22'),
+  }]; 
+}]);
