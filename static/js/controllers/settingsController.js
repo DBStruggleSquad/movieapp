0073525817c9fdc726fda18dfa830ce3582f9e1e
acotto <antoine.cotto@upr.edu>
2015-10-11 23:29:49 -0400

    Added new setting modals

--- static/js/controllers/settingsController.js
+++ static/js/controllers/settingsController.js
@@ -29,4 +29,14 @@ app.controller('settings', ['$scope', function($scope) {
     s.active = !s.active;
   };
 
+  $scope.showModal = false;
+  $scope.accType = 'Username';
+  $scope.currentOpt = 'Unknown';
+  $scope.toggleModal = function(a){
+    $scope.showModal = !$scope.showModal;
+    $scope.accType = a.type;
+    if (a.type == 'Password') {$scope.currentOpt = '**********'} else{$scope.currentOpt = a.current;};
+    
+  } 
+
 }]);
