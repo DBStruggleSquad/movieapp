2217dd28a2f0aa39acb3b54614994cfa0e4b586c
Glorimar Castro <glorimar.castro@upr.edu>
2015-11-10 21:13:36 -0400

    first insert

--- app.py
+++ app.py
@@ -408,12 +408,12 @@ def add_movie2list():
     data = request.get_json()
     print data['description'] + "  " + data['movieTitle'] + "   " + data['title'] 
     temp = json.loads(str(data['listName']))
-    print temp
-    
+    print temp['name']
     conn = mysql.connect()
     cur = conn.cursor()
+    cur.callproc('addListPost', (data['title'], temp['name'], 'Movies' , data['description'], data['movieTitle'] ))
     conn.close()
-    
+    print "salio"
     
     return jsonify({})
     
