b6c16f08b2d66b5795163cdf498dc17412c3c9bc
kborgos <katya.borgos@upr.edu>
2015-11-10 20:47:10 -0400

    test

--- static/js/controllers/movieController.js
+++ static/js/controllers/movieController.js
@@ -1,22 +1,22 @@
 //= require_tree .
 app.controller('profile', ['$scope', '$http', function($scope, $http) {
 
-	$scope.movie = $http.get("/movieinfo/" + localStorage.getItem("movieTitle")).success(function(data){
-	  	$scope.movie = data;
-	}).error(function(data){
-	  		window.alert("hola");
-	});
-	
-	
-	$scope.movieReviews = $http.get('/moviereviews/' + localStorage.getItem("movieTitle")).success(function(data){
-	  	$scope.movieReviews = data;
-	});
+  $scope.movie = $http.get("/movieinfo/" + localStorage.getItem("movieTitle")).success(function(data){
+      $scope.movie = data;
+  }).error(function(data){
+        window.alert("hola");
+  });
+  
+  
+  $scope.movieReviews = $http.get('/moviereviews/' + localStorage.getItem("movieTitle")).success(function(data){
+      $scope.movieReviews = data;
+  });
 
-	$scope.availableLists = $http.get('/usermovielistnames' ).success(function(data){
-  		$scope.availableLists = data.lists;
-  	});
+  $scope.availableLists = $http.get('/usermovielistnames' ).success(function(data){
+      $scope.availableLists = data.lists;
+    });
 
-	$scope.tabs = [{
+  $scope.tabs = [{
             title: 'Info',
             url: 'info.tpl.html'
         }, {
@@ -41,17 +41,18 @@ app.controller('profile', ['$scope', '$http', function($scope, $http) {
       $scope.showModal = !$scope.showModal;
   };        
 
-	//--------------------------
-	//FUNSIONES PARA ANADIR MOVIE A LISTA
-	//--------------------------
-	$scope.data2send = {movieTitle: "",  title: "", description: ""};
-	$scope.addMovie2List = function(moviename){
-		$scope.data2send.movieTitle = moviename;
-		$http.post('/addmovie2list', $scope.data2send).success(function(data){
-			
-		}).error(function(data){
-			window.alert(data);
-		});
-		
-	};
+  //--------------------------
+  //FUNSIONES PARA ANADIR MOVIE A LISTA
+  //--------------------------
+  $scope.data2send = {movieTitle: "",  title: "", description: "", listName: ""};
+  $scope.addMovie2List = function(moviename, selectedlistname){
+    $scope.data2send.movieTitle = moviename;
+    $scope.data2send.listName = selectedlistname;
+    $http.post('/addmovie2list', $scope.data2send).success(function(data){
+      
+    }).error(function(data){
+      window.alert(data);
+    });
+    
+  };
 }]);
