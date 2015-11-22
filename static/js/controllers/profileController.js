2bcb0554d816fab307fd1d5b1e64fcd6d6e6e444
acotto <antoine.cotto@upr.edu>
2015-11-22 17:11:54 -0400

    user text posts

--- static/js/controllers/profileController.js
+++ static/js/controllers/profileController.js
@@ -1,33 +1,33 @@
 app.controller('profile', ['$scope', '$http', function($scope,$http) {
 	
-    $scope.userDetails = {
-        name: '',
-        rating: 0,
-        quote: ""
-      };
+  $scope.userDetails = {
+    name: '',
+    rating: 0,
+    quote: ""
+  };
 
-    $scope.activities = $http.get('/myuserprofileactivity').success(function(data){
-        $scope.activities = data.activity;
-    }).error(function(data, status){
-        window.alert(data + '\n' + status);
-    });
+  $scope.activities = $http.get('/myuserprofileactivity').success(function(data){
+    $scope.activities = data.activity;
+  }).error(function(data, status){
+    window.alert(data + '\n' + status);
+  });
 
-    $scope.userLists = $http.get('/myuserlists').success(function(data){
-        $scope.userLists = data.lists;
-    });
+  $scope.userLists = $http.get('/myuserlists').success(function(data){
+    $scope.userLists = data.lists;
+  });
 
-    $scope.userRank = $http.get('/myuserank').success(function(data){
-        $scope.userDetails.rating = data["rank"];
-        $scope.userDetails.name = data["user"];
-        $scope.userDetails.quote = data["quote"];
-        $scope.userDetails.picture = data["picture"];
-    });
+  $scope.userRank = $http.get('/myuserank').success(function(data){
+    $scope.userDetails.rating = data["rank"];
+    $scope.userDetails.name = data["user"];
+    $scope.userDetails.quote = data["quote"];
+    $scope.userDetails.picture = data["picture"];
+  });
 
-      $scope.userReview = $http.get('/myuserreviews').success(function(data){
-          $scope.userReview = data.reviews;
-      }).error(function(data, status){
-          window.alert('userreview' + data + status);
-      });
+  $scope.userReview = $http.get('/myuserreviews').success(function(data){
+    $scope.userReview = data.reviews;
+  }).error(function(data, status){
+    window.alert('userreview' + data + status);
+  });
 
   //
   // To get each reviewer's rating, we only need to set a bound; users cannot give half stars.
@@ -40,25 +40,36 @@ app.controller('profile', ['$scope', '$http', function($scope,$http) {
     return new Array(parseInt($scope.userDetails.rating));
   };     
 
-$scope.tabs = [{
-            title: 'Activity',
-            url: 'activity.tpl.html'
-        }, {
-            title: 'Lists',
-            url: 'lists.tpl.html'
-        }, {
-            title: 'Reviews',
-            url: 'reviews.tpl.html'
-    }];
+  $scope.tabs = [{
+    title: 'Activity',
+    url: 'activity.tpl.html'
+  }, {
+    title: 'Lists',
+    url: 'lists.tpl.html'
+  }, {
+    title: 'Reviews',
+    url: 'reviews.tpl.html'
+  }];
 
     $scope.currentTab = 'activity.tpl.html'; //default opened tab
 
     $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.url;
+      $scope.currentTab = tab.url;
     };
     
     $scope.isActiveTab = function(tabUrl) {
-        return tabUrl == $scope.currentTab;
+      return tabUrl == $scope.currentTab;
     };
 
-}]);
+
+    $scope.data2sendPost = {title: "", post: ""};
+    $scope.adduserpost = function(){
+      $http.post('/adduserpost', $scope.data2sendPost).success(function(data){
+        location.reload(true)
+      }).error(function(data){
+        window.alert("Worst error code ever in post");
+      });
+
+    };
+
+  }]);
